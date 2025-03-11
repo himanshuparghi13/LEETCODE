bb074b5962b41920c8dbe0f06988a2004787dc53
HIMANSHU PARGHI <himanshuparghi.0710@gmail.com>
2025-03-11 13:03:27 +0530

    Time: 0 ms (100%), Space: 11.7 MB (76.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,15 @@ public:
         long long cnt=0;
         long long mx=0;
         int left = 0;
-        vector<char>mp(256 , 0);
-        for(int i=0;i<s.size();i++){
-           mp[s[i]]++;
-            while(mp[s[i]] >1) {
-                mp[s[left] ]--;
-                left++;
+        vector<int>mp(256 , -1);
+        int i = 0;
+        while(i<s.size()){
+            if(mp[s[i]] != -1 && mp[s[i]] >= left) {
+                left = max(mp[s[i]] + 1, left);
             }
+           mp[s[i]] = i;
            mx=fmax(mx,i-left+1);
+           i++;
         }
         return mx;
     }
