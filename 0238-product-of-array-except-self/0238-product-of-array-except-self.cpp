b652eb38fee291f5196ce7365ad0f31b2c40689e
HIMANSHU PARGHI <himanshuparghi.0710@gmail.com>
2025-05-27 11:13:48 +0530

    Time: 0 ms (100%), Space: 42.3 MB (9.17%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
         vector<int>px(nums.size());
-        px[0] = nums[0];
-        for(int i=1;i<nums.size()-1;i++){
-            px[i] = px[i-1]*nums[i];
-        }
         vector<int>sx(nums.size());
+        px[0] = nums[0];
         sx[nums.size()-1] = nums[nums.size()-1];
-        for(int i=nums.size()-2;i>=0;i--){
-            sx[i] = sx[i+1]*nums[i];
-        }
-        vector<int>vv(nums.size());
-        vv[0] = sx[1];
         for(int i=1;i<nums.size()-1;i++){
-            vv[i] = px[i-1]* sx[i+1];
+            px[i] = px[i-1]* nums[i];
+            sx[nums.size()-i-1] = sx[nums.size()-i]*nums[nums.size()-i-1]; 
         }
-        vv[nums.size()-1] = px[nums.size()-2]; 
-        return vv;
+        px[nums.size()-1] = px[nums.size()-2]*nums[nums.size()-1];
+        sx[0] = sx[1]*nums[1];
+        vector<int>v(nums.size());
+        v[0]= sx[1];
+        v[nums.size()-1] = px[nums.size()-2];
+        for(int i=1;i<nums.size()-1;i++){
+            v[i] = px[i-1]*sx[i+1];
+        }
+        return v;
     }
 };
