5f35f9cab7eeb087648dbb84e7430113aeceb9cd
HIMANSHU PARGHI <himanshuparghi.0710@gmail.com>
2025-09-12 10:21:25 +0530

    Time: 6 ms (63.25%), Space: 19.1 MB (25.75%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int cnt = 0;
+        for(int i =0;i<s.size();i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i'|| s[i] == 'o' || s[i] == 'u') cnt++;
+        }
+        return cnt;
+    }
+};
