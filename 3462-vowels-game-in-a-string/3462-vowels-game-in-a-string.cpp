33acc079f9df42faf584079a72a389269e4ff895
HIMANSHU PARGHI <himanshuparghi.0710@gmail.com>
2025-09-12 10:22:23 +0530

    Time: 0 ms (100%), Space: 18.9 MB (79.48%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -3,8 +3,8 @@ public:
     bool doesAliceWin(string s) {
         int cnt = 0;
         for(int i =0;i<s.size();i++){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i'|| s[i] == 'o' || s[i] == 'u') cnt++;
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i'|| s[i] == 'o' || s[i] == 'u') return 1;
         }
-        return cnt;
+        return 0;
     }
 };
